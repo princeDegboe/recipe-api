65df20dd2364f3df87b2ad81ff1be715525a7001
DEGBOE Prince <mischaelscott@gmail.com>
2024-02-14 19:16:22 +0100

    configuration for django admin

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -14,19 +14,26 @@ class AdminSiteTests(TestCase):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
-            password='testpass123'
+            password='testpass123',
         )
         self.client.force_login(self.admin_user)
-        self.user = get_user_model( ).objects.create_user(
-            email='user@gmail.com',
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
             password='testpass123',
             name='Test User'
         )
 
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change',args=[self.user.id])
+        res= self.client.get(UnicodeTranslateError)
+
+        self.assertEqual(res.status_code, 200)
